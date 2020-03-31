86482a4f51968798a07cd4a5cb00386d1c521cea
manjinder98 <manjindermultani1998@gmail.com>
2020-03-31 18:10:42 +0100

    Add validation to Comments model

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,12 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(params[:comment].permit(:name, :comment))
-    redirect_to post_path(@post)
+
+    if @comment
+      redirect_to post_path(@post)
+    else
+      render 'posts/show'
+    end
   end
 
   def destroy
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
+  validates :name, :comment, presence: true
+  validates :name, length: { maximum: 20, too_long: "%{count} characters is the maximum allowed" }, uniqueness: true
   belongs_to :post
 end
