989f10c8526bc648cca8c1a08133f9dc38f2dc4e
Manjinder <manjinder.singh.2018@strath.ac.uk>
2020-03-31 15:50:49 +0100

    Add validation to form

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+  validates :title, :content, presence: true
+  validates :title, length: { maximum: 60, too_long: "%{count} characters is the maximum allowed" }, uniqueness: true
 end
