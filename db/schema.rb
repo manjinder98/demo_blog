8df0fc3f034eea790f7573151ef6d0c8285732e2
Manjinder <manjinder.singh.2018@strath.ac.uk>
2020-04-01 17:27:25 +0100

    Change DB from sqlite to pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_03_31_150058) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string "name"
     t.text "comment"
