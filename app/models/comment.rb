de56dc2b13d7887dec90a2325ab9b34fc28f1adf
manjinder <manjinder@yova.ch>
2020-04-03 13:32:22 +0100

    Fix uniquess validation username comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :post
   validates :name, :comment, presence: true
-  validates :name, length: { maximum: 20, too_long: "%{count} characters is the maximum allowed" }, uniqueness: true
+  validates :name, length: { maximum: 20, too_long: "%{count} characters is the maximum allowed" }, uniqueness: { scope: :post}
 end
