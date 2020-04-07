6186027d60148b160a813cd584ad1e7fd3435ab4
manjinder <manjinder@yova.ch>
2020-04-07 08:56:27 +0100

    Add rails_admin.rb

---
+++ config/initializers/rails_admin.rb
@@ -0,0 +1,49 @@
+require 'i18n'
+I18n.default_locale = :en
+
+RailsAdmin.config do |config|
+
+  ### Popular gems integration
+  config.main_app_name = "Demo Blog"
+
+  config.label_methods << :description # Default is [:name, :title]
+
+  config.browser_validations = false # Default is true
+  ## == Devise ==
+  # config.authenticate_with do
+  #   warden.authenticate! scope: :user
+  # end
+  # config.current_user_method(&:current_user)
+
+  ## == CancanCan ==
+  # config.authorize_with :cancancan
+
+  ## == Pundit ==
+  # config.authorize_with :pundit
+
+  ## == PaperTrail ==
+  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
+
+  ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
+
+  ## == Gravatar integration ==
+  ## To disable Gravatar integration in Navigation Bar set to false
+  # config.show_gravatar = true
+
+  config.actions do
+    # root actions
+    dashboard                     # mandatory
+    # collection actions
+    index                         # mandatory
+    new
+    export
+    history_index
+    bulk_delete
+    # member actions
+    show
+    edit
+    delete
+    history_show
+    show_in_app
+  end
+end
