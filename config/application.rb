d91495e179a10c5f93d27c40500f87d066589c7b
Manjinder <manjinder.singh.2018@strath.ac.uk>
2020-04-12 21:03:13 +0100

    Generate .env file with dot-env rails

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+Dotenv::Railtie.load
 
 module DemoBlog
   class Application < Rails::Application
@@ -15,11 +16,5 @@ module DemoBlog
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.before_configuration do
-      env_file = File.join(Rails.root, 'config', 'local_env.yml')
-      YAML.load(File.open(env_file)).each do |key, value|
-        ENV[key.to_s] = value
-      end if File.exists?(env_file)
-    end
   end
 end
